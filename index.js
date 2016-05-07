18ef778d0ac79ed1dddc71b55d5db3b7d07ad12f
benb116 <benb116@gmail.com>
2016-05-07 01:28:52 -0400

    Small fixes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fileUpload = require('express-fileupload');
 
 var app = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 // app.use(bodyParser.text());
 app.use(fileUpload());
